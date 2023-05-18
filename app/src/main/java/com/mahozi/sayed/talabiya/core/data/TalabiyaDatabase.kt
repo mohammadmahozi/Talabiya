6a49bb7ab2e776ce607b275f93a628690a4f914d
Mohammad <mohammadmahozi@gmail.com>
2023-05-18 23:26:55 +0300

    Convert TalabiyaDatabase to kotlin

--- app/src/main/java/com/mahozi/sayed/talabiya/core/data/TalabiyaDatabase.kt
+++ app/src/main/java/com/mahozi/sayed/talabiya/core/data/TalabiyaDatabase.kt
@@ -1,47 +1,55 @@
-package com.mahozi.sayed.talabiya.core.data;
+package com.mahozi.sayed.talabiya.core.data
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.mahozi.sayed.talabiya.order.store.OrderDao
+import com.mahozi.sayed.talabiya.order.store.OrderEntity
+import com.mahozi.sayed.talabiya.order.store.OrderItemEntity
+import com.mahozi.sayed.talabiya.order.store.SubOrderEntity
+import com.mahozi.sayed.talabiya.person.store.PersonDao
+import com.mahozi.sayed.talabiya.person.store.PersonEntity
+import com.mahozi.sayed.talabiya.resturant.store.MenuItemEntity
+import com.mahozi.sayed.talabiya.resturant.store.RestaurantDao
+import com.mahozi.sayed.talabiya.resturant.store.RestaurantEntity
 
-import android.content.Context;
+@Database(
+  entities = [
+    OrderEntity::class,
+    OrderItemEntity::class,
+    PersonEntity::class,
+    SubOrderEntity::class,
+    MenuItemEntity::class,
+    RestaurantEntity::class
+  ],
+  version = 1
+)
+abstract class TalabiyaDatabase : RoomDatabase() {
 
-import com.mahozi.sayed.talabiya.person.store.PersonDao;
-import com.mahozi.sayed.talabiya.person.store.PersonEntity;
-import com.mahozi.sayed.talabiya.resturant.store.MenuItemEntity;
-import com.mahozi.sayed.talabiya.resturant.store.RestaurantDao;
-import com.mahozi.sayed.talabiya.resturant.store.RestaurantEntity;
-import com.mahozi.sayed.talabiya.order.store.OrderDao;
-import com.mahozi.sayed.talabiya.order.store.OrderEntity;
-import com.mahozi.sayed.talabiya.order.store.OrderItemEntity;
-import com.mahozi.sayed.talabiya.order.store.SubOrderEntity;
+  abstract fun orderDao(): OrderDao
+  abstract fun personDao(): PersonDao
+  abstract fun restaurantDao(): RestaurantDao
 
-import androidx.room.Database;
-import androidx.room.Room;
-import androidx.room.RoomDatabase;
+  companion object {
+    @Volatile
+    private var INSTANCE: TalabiyaDatabase? = null
 
-@Database(entities = {OrderEntity.class, OrderItemEntity.class, PersonEntity.class,
-        SubOrderEntity.class, MenuItemEntity.class, RestaurantEntity.class}, version = 1)
-
-public abstract class TalabiyaDatabase extends RoomDatabase {
-
-        public abstract OrderDao orderDao();
-        public abstract PersonDao personDao();
-        public abstract RestaurantDao restaurantDao();
-
-
-
-
-        private static volatile TalabiyaDatabase INSTANCE;
-
-        public static TalabiyaDatabase getDatabase(final Context context) {
-            if (INSTANCE == null) {
-                synchronized (TalabiyaDatabase.class) {
-                    if (INSTANCE == null) {
-                        INSTANCE = Room.databaseBuilder(context.getApplicationContext(), TalabiyaDatabase.class, "talabiya_database")
-                                .createFromAsset("talabiya_database").allowMainThreadQueries().build();
-                    }
-                }
-            }
-
-            return INSTANCE;
+    @JvmStatic
+    fun getDatabase(context: Context): TalabiyaDatabase {
+      if (INSTANCE == null) {
+        synchronized(TalabiyaDatabase::class.java) {
+          if (INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(
+              context.applicationContext,
+              TalabiyaDatabase::class.java,
+              "talabiya_database"
+            )
+              .createFromAsset("talabiya_database").allowMainThreadQueries().build()
+          }
         }
-
+      }
+      return INSTANCE!!
+    }
+  }
 }
