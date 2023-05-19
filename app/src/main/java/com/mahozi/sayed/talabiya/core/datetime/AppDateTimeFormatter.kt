3b0ea7fdc9815a172dc020a2b5239f47fae580b5
Mohammad <mohammadmahozi@gmail.com>
2023-05-19 15:33:56 +0300

    Use zone with formatter

--- app/src/main/java/com/mahozi/sayed/talabiya/core/datetime/AppDateTimeFormatter.kt
+++ app/src/main/java/com/mahozi/sayed/talabiya/core/datetime/AppDateTimeFormatter.kt
@@ -1,56 +1,62 @@
 package com.mahozi.sayed.talabiya.core.datetime
 
 import android.app.Activity
-import android.content.Context
 import androidx.compose.runtime.staticCompositionLocalOf
 import com.mahozi.sayed.talabiya.core.extensions.locale
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.time.temporal.TemporalAccessor
 import javax.inject.Inject
 
 val LocalDateTimeFormatter = staticCompositionLocalOf<AppDateTimeFormatter> {
-    error("AppLocalDateTimeFormatter is not provider")
+  error("AppLocalDateTimeFormatter is not provider")
 }
 
 class AppDateTimeFormatter @Inject constructor(
-    private val context: Activity
+  private val context: Activity
 ) {
 
-    private val shortDateTimeFormatter: DateTimeFormatter =
-        DateTimeFormatter.ofPattern(" yyyy/MM/dd - hh:mm a", context.locale)
+  private val zoneId = ZoneId.systemDefault()
 
-    private val shortDateFormatter: DateTimeFormatter =
-        DateTimeFormatter.ofPattern("yyyy/MM/dd", context.locale)
+  private val shortDateTimeFormatter: DateTimeFormatter =
+    DateTimeFormatter.ofPattern(" yyyy/MM/dd - hh:mm a", context.locale)
+      .withZone(zoneId)
 
-    private val shortDateWithDayFormatter: DateTimeFormatter =
-        DateTimeFormatter.ofPattern("EEEE yyyy/MM/dd", context.locale)
+  private val shortDateFormatter: DateTimeFormatter =
+    DateTimeFormatter.ofPattern("yyyy/MM/dd", context.locale)
+      .withZone(zoneId)
 
-    private val timeFormatter: DateTimeFormatter =
-        DateTimeFormatter.ofPattern("hh:mm a", context.locale)
+  private val shortDateWithDayFormatter: DateTimeFormatter =
+    DateTimeFormatter.ofPattern("EEEE yyyy/MM/dd", context.locale)
+      .withZone(zoneId)
 
-    /**
-     * Format date to 2022/01/24
-     */
-    fun formatShortDate(temporal: TemporalAccessor): String = shortDateFormatter.format(temporal)
+  private val timeFormatter: DateTimeFormatter =
+    DateTimeFormatter.ofPattern("hh:mm a", context.locale)
+      .withZone(zoneId)
 
-    /**
-     * Format date to Sunday 2022/01/24
-     */
-    fun formatShortDateWithDay(temporal: TemporalAccessor): String = shortDateWithDayFormatter.format(temporal)
+  /**
+   * Format date to 2022/01/24
+   */
+  fun formatShortDate(temporal: TemporalAccessor): String = shortDateFormatter.format(temporal)
 
-    /**
-     * Format time to 02:30 PM
-     */
-    fun formatTime(temporal: TemporalAccessor): String = timeFormatter.format(temporal)
+  /**
+   * Format date to Sunday 2022/01/24
+   */
+  fun formatShortDateWithDay(temporal: TemporalAccessor): String = shortDateWithDayFormatter.format(temporal)
 
-    /**
-     * Format date time to 2022/01/24 - 12:00 PM
-     */
-    fun formatShortDateTime(temporal: TemporalAccessor): String = shortDateTimeFormatter.format(temporal)
+  /**
+   * Format time to 02:30 PM
+   */
+  fun formatTime(temporal: TemporalAccessor): String = timeFormatter.format(temporal)
 
-    /**
-     * Format date and time to 2022/01/24 - 12:00 PM
-     */
-    fun formatShortDateTime(date: TemporalAccessor, time: TemporalAccessor): String =
-        "${formatShortDate(date)} - ${formatTime(time)}"
+  /**
+   * Format date time to 2022/01/24 - 12:00 PM
+   */
+  fun formatShortDateTime(temporal: TemporalAccessor): String = shortDateTimeFormatter.format(temporal)
+
+  /**
+   * Format date and time to 2022/01/24 - 12:00 PM
+   */
+  fun formatShortDateTime(date: TemporalAccessor, time: TemporalAccessor): String =
+    "${formatShortDate(date)} - ${formatTime(time)}"
 }
