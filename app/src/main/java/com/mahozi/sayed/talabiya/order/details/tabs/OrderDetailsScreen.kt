3a65c18ba31f1970471f1cda5cb8bc8ca9154dda
Mohammad <mohammadmahozi@gmail.com>
2023-05-20 01:46:25 +0300

    Add top bar to order details

--- app/src/main/java/com/mahozi/sayed/talabiya/order/details/tabs/OrderDetailsScreen.kt
+++ app/src/main/java/com/mahozi/sayed/talabiya/order/details/tabs/OrderDetailsScreen.kt
@@ -1,84 +1,93 @@
 package com.mahozi.sayed.talabiya.order.details.tabs
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Tab
-import androidx.compose.material.TabRow
-import androidx.compose.material.TabRowDefaults
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import com.mahozi.sayed.talabiya.R
 import com.mahozi.sayed.talabiya.core.navigation.Screen
+import com.mahozi.sayed.talabiya.core.ui.TalabiyaBar
 import com.mahozi.sayed.talabiya.core.ui.string
 import com.mahozi.sayed.talabiya.core.ui.theme.colors
 import com.mahozi.sayed.talabiya.order.details.info.OrderInfoScreen
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class OrderDetailsScreen(val orderId: Int): Screen
+data class OrderDetailsScreen(val orderId: Int) : Screen
 
-@Composable fun OrderDetailsUi(state: OrderDetailsState, onEvent: (OrderDetailsEvent) -> Unit) {
-    var selectedTabIndex by remember { mutableStateOf(0) }
-    val tab = OrderDetailsTab.tab(selectedTabIndex)
+@Composable
+fun OrderDetailsUi(state: OrderDetailsState, onEvent: (OrderDetailsEvent) -> Unit) {
+  Scaffold(
+    topBar = { TalabiyaBar(R.string.order_number) }
+  ) {
+    Box(
+      modifier = Modifier.padding(it)
+    ) {
+      var selectedTabIndex by remember { mutableStateOf(0) }
+      val tab = OrderDetailsTab.tab(selectedTabIndex)
 
-    Column {
+      Column {
         Tabs(
-            selectedTabIndex = selectedTabIndex,
-            onTabSelected = { selectedTabIndex = it }
+          selectedTabIndex = selectedTabIndex,
+          onTabSelected = { selectedTabIndex = it }
         )
 
         when (tab) {
-            OrderDetailsTab.INFO -> if (state.info != null) OrderInfoScreen(state.info, onEvent)
-            OrderDetailsTab.SUBORDERS -> {}
-            OrderDetailsTab.FULL -> {}
+          OrderDetailsTab.INFO -> if (state.info != null) OrderInfoScreen(state.info, onEvent)
+          OrderDetailsTab.SUBORDERS -> {}
+          OrderDetailsTab.FULL -> {}
         }
+      }
     }
-
+  }
 }
 
-@Composable fun Tabs(
-    selectedTabIndex: Int,
-    onTabSelected: (index: Int) -> Unit
+@Composable
+fun Tabs(
+  selectedTabIndex: Int,
+  onTabSelected: (index: Int) -> Unit
 ) {
-    TabRow(
-        selectedTabIndex = selectedTabIndex,
-        backgroundColor = colors.rowBackground,
-        contentColor = colors.primaryText,
-        indicator = @Composable { tabPositions ->
-            TabRowDefaults.Indicator(
-                Modifier
-                    .tabIndicatorOffset(tabPositions[selectedTabIndex]),
-                color = colors.primary
-            )
-        },
-    ) {
-        Tab(
-            selected = false,
-            onClick = { onTabSelected(0) },
-            text = {
-                Text(
-                    text = string(R.string.info)
-                )
-            }
+  TabRow(
+    selectedTabIndex = selectedTabIndex,
+    backgroundColor = colors.rowBackground,
+    contentColor = colors.primaryText,
+    indicator = @Composable { tabPositions ->
+      TabRowDefaults.Indicator(
+        Modifier
+          .tabIndicatorOffset(tabPositions[selectedTabIndex]),
+        color = colors.primary
+      )
+    },
+  ) {
+    Tab(
+      selected = false,
+      onClick = { onTabSelected(0) },
+      text = {
+        Text(
+          text = string(R.string.info)
         )
-        Tab(
-            selected = false,
-            onClick = { onTabSelected(1) },
-            text = {
-                Text(
-                    text = string(R.string.suborders)
-                )
-            }
+      }
+    )
+    Tab(
+      selected = false,
+      onClick = { onTabSelected(1) },
+      text = {
+        Text(
+          text = string(R.string.suborders)
         )
-        Tab(
-            selected = false,
-            onClick = { onTabSelected(2) },
-            text = {
-                Text(
-                    text = string(R.string.full_order)
-                )
-            }
+      }
+    )
+    Tab(
+      selected = false,
+      onClick = { onTabSelected(2) },
+      text = {
+        Text(
+          text = string(R.string.full_order)
         )
-    }
+      }
+    )
+  }
 }
