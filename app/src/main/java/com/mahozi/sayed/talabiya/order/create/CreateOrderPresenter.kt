cd41155176d5a27f57e541e9f3bf948108d6949c
Mohammad <mohammadmahozi@gmail.com>
2023-06-16 03:14:47 +0300

    Create order using correct restaurant

--- app/src/main/java/com/mahozi/sayed/talabiya/order/create/CreateOrderPresenter.kt
+++ app/src/main/java/com/mahozi/sayed/talabiya/order/create/CreateOrderPresenter.kt
@@ -1,6 +1,7 @@
 package com.mahozi.sayed.talabiya.order.create
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -9,6 +10,7 @@ import com.mahozi.sayed.talabiya.core.CollectEvents
 import com.mahozi.sayed.talabiya.core.Presenter
 import com.mahozi.sayed.talabiya.core.navigation.Navigator
 import com.mahozi.sayed.talabiya.order.store.OrderStore
+import com.mahozi.sayed.talabiya.resturant.store.RestaurantStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 import restaurant.RestaurantEntity
@@ -18,11 +20,14 @@ import javax.inject.Inject
 
 class CreateOrderPresenter @Inject constructor(
   private val orderStore: OrderStore,
+  private val restaurantStore: RestaurantStore,
   private val navigator: Navigator,
 ): Presenter<CreateOrderEvent, CreateOrderState> {
 
   @Composable
   override fun start(events: Flow<CreateOrderEvent>): CreateOrderState {
+    val restaurants by remember { restaurantStore.restaurants }.collectAsState(initial = emptyList())
+
     var selectedRestaurant by remember { mutableStateOf<RestaurantEntity?>(null) }
     var selectedDate by remember { mutableStateOf(LocalDate.now()) }
     var selectedTime by remember { mutableStateOf(LocalTime.now()) }
@@ -45,11 +50,7 @@ class CreateOrderPresenter @Inject constructor(
 
     return CreateOrderState(
       selectedRestaurant,
-      listOf(
-        RestaurantEntity(0L, "Restaurant 1"),
-        RestaurantEntity(0L, "Restaurant 2"),
-        RestaurantEntity(0L, "Restaurant 3"),
-      ),
+      restaurants,
       LocalDate.now(),
       LocalTime.now()
     )
