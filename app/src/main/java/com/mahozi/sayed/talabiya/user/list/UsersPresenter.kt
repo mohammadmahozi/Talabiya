cbfc59a988b4ea59e7d50a6e5f47c49a8a0f8241
Mohammad <MohammadMahozi@gmail.com>
2023-10-14 20:50:43 +0300

    Enable navigating to create user screen

--- app/src/main/java/com/mahozi/sayed/talabiya/user/list/UsersPresenter.kt
+++ app/src/main/java/com/mahozi/sayed/talabiya/user/list/UsersPresenter.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.remember
 import com.mahozi.sayed.talabiya.core.CollectEvents
 import com.mahozi.sayed.talabiya.core.Presenter
 import com.mahozi.sayed.talabiya.core.navigation.Navigator
+import com.mahozi.sayed.talabiya.user.create.CreateUserScreen
 import com.mahozi.sayed.talabiya.user.data.UserStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
@@ -22,7 +23,7 @@ class UsersPresenter @Inject constructor(
 
     CollectEvents(events) { event ->
       when(event) {
-        UsersEvent.CreateUserClicked -> {}/*navigator.goto(CreateUserScreen)*/
+        UsersEvent.CreateUserClicked -> navigator.goto(CreateUserScreen)
         is UsersEvent.DeleteUserClicked -> {
           launch {
             userStore.delete(event.user.id)
