4bac40b0a23a659c33032c935c4504c9ad9c04dc
Mohammad <MohammadMahozi@gmail.com>
2025-12-28 22:29:17 +0300

    Mark presenter as staple to disable warning

--- app/src/main/java/com/mahozi/sayed/talabiya/core/Presenter.kt
+++ app/src/main/java/com/mahozi/sayed/talabiya/core/Presenter.kt
@@ -1,12 +1,14 @@
 package com.mahozi.sayed.talabiya.core
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import com.mahozi.sayed.talabiya.core.navigation.Screen
 import kotlinx.coroutines.flow.Flow
 
 /**
  * Takes a stream of events and produces state over time for a [Screen].
  */
+@Stable
 interface Presenter<Event, State> {
   @Composable
   fun start(events: Flow<Event>): State
