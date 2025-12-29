4932955edc02e2fea22709485f0d61f54b07b35f
Mohammad <MohammadMahozi@gmail.com>
2025-12-29 23:00:22 +0300

    Fill screen size in order info

--- app/src/main/java/com/mahozi/sayed/talabiya/order/details/info/OrderInfoScreen.kt
+++ app/src/main/java/com/mahozi/sayed/talabiya/order/details/info/OrderInfoScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -60,12 +61,13 @@ fun OrderInfoScreen(
   onEvent: (OrderInfoEvent) -> Unit,
   modifier: Modifier = Modifier
 ) {
-
   val formatter = LocalDateTimeFormatter.current
 
   Column(
     verticalArrangement = Arrangement.spacedBy(8.dp),
-    modifier = modifier.padding(16.dp)
+    modifier = modifier
+      .fillMaxSize()
+      .padding(16.dp)
   ) {
     InfoTextRow(
       text = formatter.formatShortDateWithDay(model.datetime),
