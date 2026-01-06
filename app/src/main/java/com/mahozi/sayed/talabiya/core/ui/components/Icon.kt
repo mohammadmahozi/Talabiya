611c843668b9e9da4c33d4404e53918f240966d6
Mohammad <MohammadMahozi@gmail.com>
2026-01-07 02:00:16 +0300

    Add TlbIcon

--- app/src/main/java/com/mahozi/sayed/talabiya/core/ui/components/Icon.kt
+++ app/src/main/java/com/mahozi/sayed/talabiya/core/ui/components/Icon.kt
@@ -1,14 +1,51 @@
 package com.mahozi.sayed.talabiya.core.ui.components
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpSize
+import com.mahozi.sayed.talabiya.R
+import com.mahozi.sayed.talabiya.core.ui.theme.AppTheme
 
+
+private val LocalIconSize = staticCompositionLocalOf {
+  DpSize(Dp.Unspecified, Dp.Unspecified)
+}
+
+@Preview
+@Composable
+private fun PreviewTlbIcon() {
+  AppTheme {
+    TlbIcon(
+      painter = painterResource(R.drawable.ic_payer),
+      contentDescription = null,
+    )
+  }
+}
+@Composable
+fun TlbIcon(
+  painter: Painter,
+  contentDescription: String?,
+  modifier: Modifier = Modifier,
+  tint: Color = LocalContentColor.current,
+) {
+  Icon(
+    painter = painter,
+    contentDescription = contentDescription,
+    modifier = modifier.size(LocalIconSize.current),
+    tint = tint,
+  )
+}
 
 @Composable
 fun TalabiyaIconButton(
