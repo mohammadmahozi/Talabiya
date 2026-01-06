9981d4c2b1bbddbbaef8527726d545ce3a58b829
Mohammad <MohammadMahozi@gmail.com>
2026-01-07 02:42:07 +0300

    Add TlbTextField and TlbSelectionField

--- app/src/main/java/com/mahozi/sayed/talabiya/core/ui/components/TextField.kt
+++ app/src/main/java/com/mahozi/sayed/talabiya/core/ui/components/TextField.kt
@@ -1,16 +1,24 @@
 package com.mahozi.sayed.talabiya.core.ui.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.input.pointer.PointerEventPass
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.VisualTransformation
 
@@ -43,26 +51,137 @@ fun TalabiyaOutlinedTextField(
   OutlinedTextField(
     value = value,
     onValueChange = onValueChange,
-    modifier,
-    enabled,
-    readOnly,
-    textStyle,
-    label,
-    placeholder,
-    leadingIcon,
-    trailingIcon,
-    prefix,
-    suffix,
-    supportingText,
-    isError,
-    visualTransformation,
-    keyboardOptions,
-    keyboardActions,
-    singleLine,
-    maxLines,
-    minLines,
-    interactionSource,
-    shape,
-    colors
+    modifier = modifier,
+    enabled = enabled,
+    readOnly = readOnly,
+    textStyle = textStyle,
+    label = label,
+    placeholder = placeholder,
+    leadingIcon = leadingIcon,
+    trailingIcon = trailingIcon,
+    prefix = prefix,
+    suffix = suffix,
+    supportingText = supportingText,
+    isError = isError,
+    visualTransformation = visualTransformation,
+    keyboardOptions = keyboardOptions,
+    keyboardActions = keyboardActions,
+    singleLine = singleLine,
+    maxLines = maxLines,
+    minLines = minLines,
+    interactionSource = interactionSource,
+    shape = shape,
+    colors = colors
+  )
+}
+
+@Composable
+fun TlbTextField(
+  value: String,
+  onValueChange: (String) -> Unit,
+  modifier: Modifier = Modifier,
+  enabled: Boolean = true,
+  readOnly: Boolean = false,
+  textStyle: TextStyle = LocalTextStyle.current,
+  label: @Composable (() -> Unit)? = null,
+  placeholder: @Composable (() -> Unit)? = null,
+  leadingIcon: @Composable (() -> Unit)? = null,
+  trailingIcon: @Composable (() -> Unit)? = null,
+  prefix: @Composable (() -> Unit)? = null,
+  suffix: @Composable (() -> Unit)? = null,
+  supportingText: @Composable (() -> Unit)? = null,
+  error: String? = null,
+  visualTransformation: VisualTransformation = VisualTransformation.None,
+  keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+  keyboardActions: KeyboardActions = KeyboardActions.Default,
+  singleLine: Boolean = false,
+  maxLines: Int = if (singleLine) 1 else Int.MAX_VALUE,
+  minLines: Int = 1,
+  interactionSource: MutableInteractionSource? = null,
+  shape: Shape = TextFieldDefaults.shape,
+  colors: TextFieldColors = TextFieldDefaults.colors(),
+) {
+  TextField(
+    value = value,
+    onValueChange = onValueChange,
+    modifier = modifier,
+    enabled = enabled,
+    readOnly = readOnly,
+    textStyle = textStyle,
+    label = label,
+    placeholder = placeholder,
+    leadingIcon = leadingIcon,
+    trailingIcon = trailingIcon,
+    prefix = prefix,
+    suffix = suffix,
+    supportingText = supportingText,
+    isError = error != null,
+    visualTransformation = visualTransformation,
+    keyboardOptions = keyboardOptions,
+    keyboardActions = keyboardActions,
+    singleLine = singleLine,
+    maxLines = maxLines,
+    minLines = minLines,
+    interactionSource = interactionSource,
+    shape = shape,
+    colors = colors
+  )
+}
+
+@Composable
+fun TlbSelectionField(
+  value: String,
+  onClick: () -> Unit,
+  modifier: Modifier = Modifier,
+  enabled: Boolean = true,
+  textStyle: TextStyle = LocalTextStyle.current,
+  label: @Composable (() -> Unit)? = null,
+  placeholder: @Composable (() -> Unit)? = null,
+  leadingIcon: @Composable (() -> Unit)? = null,
+  trailingIcon: @Composable (() -> Unit)? = null,
+  prefix: @Composable (() -> Unit)? = null,
+  suffix: @Composable (() -> Unit)? = null,
+  supportingText: @Composable (() -> Unit)? = null,
+  error: String? = null,
+  visualTransformation: VisualTransformation = VisualTransformation.None,
+  keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+  keyboardActions: KeyboardActions = KeyboardActions.Default,
+  interactionSource: MutableInteractionSource? = null,
+  shape: Shape = TextFieldDefaults.shape,
+  colors: TextFieldColors = TextFieldDefaults.colors(),
+) {
+  TlbTextField(
+    value = value,
+    onValueChange = {},
+    modifier = modifier
+      .pointerInput(Unit) {
+        awaitEachGesture {
+          awaitFirstDown(pass = PointerEventPass.Initial)
+          val upEvent = waitForUpOrCancellation(pass = PointerEventPass.Initial)
+          if (upEvent != null) {
+            onClick()
+          }
+        }
+      }.clickable {},
+    enabled = enabled,
+    readOnly = true,
+    textStyle = textStyle,
+    label = label,
+    error = error,
+    placeholder = placeholder,
+    leadingIcon = leadingIcon,
+    trailingIcon = trailingIcon,
+    prefix = prefix,
+    suffix = suffix,
+    supportingText = supportingText,
+    visualTransformation = visualTransformation,
+    keyboardOptions = keyboardOptions,
+    keyboardActions = keyboardActions,
+    interactionSource = interactionSource,
+    shape = shape,
+    colors = colors,
+    singleLine = true,
+    maxLines = 1,
+    minLines = 1
   )
 }
