bd8436b7233b62253994759bc3f24e1a54f32892
Mohammad <MohammadMahozi@gmail.com>
2026-02-06 19:15:16 +0300

    Formatting

--- app/src/main/java/com/mahozi/sayed/talabiya/core/data/DataModule.kt
+++ app/src/main/java/com/mahozi/sayed/talabiya/core/data/DataModule.kt
@@ -1,5 +1,6 @@
 package com.mahozi.sayed.talabiya.core.data
 
+import android.R.attr.name
 import android.content.Context
 import androidx.sqlite.db.SupportSQLiteDatabase
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
@@ -19,26 +20,27 @@ import restaurant.MenuItemPriceEntity
 @ContributesTo(AppScope::class)
 interface DataModule {
 
-    @SingleIn(AppScope::class)
-    @Provides
-    fun provideTalabiyaDatabase(context: Context): TalabiyaDatabase {
-        val driver = AndroidSqliteDriver(
-            schema = TalabiyaDatabase.Schema,
-            context = context,
-            name = "main",
-            callback = object : AndroidSqliteDriver.Callback(TalabiyaDatabase.Schema) {
-                override fun onOpen(db: SupportSQLiteDatabase) {
-                    db.execSQL("PRAGMA foreign_keys=ON;");
-                }
-            })
-        return TalabiyaDatabase(
-          driver,
-          MenuItemPriceEntity.Adapter(TypeAdapters.instantAdapter),
-          OrderEntity.Adapter(TypeAdapters.instantAdapter),
-          OrderItemPriceEntity.Adapter(TypeAdapters.instantAdapter),
-          PaymentEntity.Adapter(TypeAdapters.instantAdapter),
-        )
-    }
+  @SingleIn(AppScope::class)
+  @Provides
+  fun provideTalabiyaDatabase(context: Context): TalabiyaDatabase {
+    val driver = AndroidSqliteDriver(
+      schema = TalabiyaDatabase.Schema,
+      context = context,
+      name = "main",
+      callback = object : AndroidSqliteDriver.Callback(TalabiyaDatabase.Schema) {
+        override fun onOpen(db: SupportSQLiteDatabase) {
+          db.execSQL("PRAGMA foreign_keys=ON;");
+        }
+      })
+    return TalabiyaDatabase(
+      driver,
+      MenuItemPriceEntity.Adapter(TypeAdapters.instantAdapter),
+      OrderEntity.Adapter(TypeAdapters.instantAdapter),
+      OrderItemPriceEntity.Adapter(TypeAdapters.instantAdapter),
+      PaymentEntity.Adapter(TypeAdapters.instantAdapter),
+    )
+  }
 
-    @Provides fun provideDispatcher(): CoroutineDispatcher = Dispatchers.IO
+  @Provides
+  fun provideDispatcher(): CoroutineDispatcher = Dispatchers.IO
 }
