058fafc736088429d92f61305152b13cce40e873
Mohammad <MohammadMahozi@gmail.com>
2026-03-20 17:10:05 +0300

    Seed only debug

--- android/app/src/main/java/com/mahozi/sayed/talabiya/core/data/DataModule.kt
+++ android/app/src/main/java/com/mahozi/sayed/talabiya/core/data/DataModule.kt
@@ -6,6 +6,7 @@ import app.cash.sqldelight.db.QueryResult
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.db.SqlSchema
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
+import com.mahozi.sayed.talabiya.BuildConfig
 import com.mahozi.sayed.talabiya.core.di.AppScope
 import com.mahozi.sayed.talabiya.core.money
 import com.mahozi.sayed.talabiya.payment.PaymentDirection
@@ -63,159 +64,164 @@ private object Schema : SqlSchema<QueryResult.Value<Unit>> by TalabiyaDatabase.S
       OrderItemPriceEntityAdapter = OrderItemPriceEntity.Adapter(TypeAdapters.instantAdapter),
       PaymentEntityAdapter = PaymentEntity.Adapter(TypeAdapters.instantAdapter),
     ).apply {
-      fun Any.id() = orderQueries.lastInsertRowId().executeAsOne()
-
-      val user1 = userQueries.insert(name = "User 1").id()
-      val user2 = userQueries.insert(name = "User 2").id()
-      val user3 =  userQueries.insert(name = "User 3").id()
-
-      restaurantQueries.insert(name = "Restaurant 1")
-
-      menuItemQueries.insert(restaurantId = 1, name = "Item 1", category = "Pastry")
-      menuItemQueries.insert(restaurantId = 1, name = "Item 2", category = "Pastry")
-      menuItemQueries.insert(restaurantId = 1, name = "Item 3", category = "Pastry")
-
-      val menu1 = menuItemQueries.insertPrice(
-        menuItemId = 1,
-        datetime = Instant.now(),
-        price = 10.money.toCents()
-      ).id()
-
-      val menu2 = menuItemQueries.insertPrice(
-        menuItemId = 2,
-        datetime = Instant.now(),
-        price = 5.money.toCents()
-      ).id()
-
-      //Both users ordered, user1 paid for whole order.
-      val order1 = orderQueries.insert(restaurantId = 1, createdAt = Instant.now()).id()
-
-      orderQueries.updatePayer(user1, order1)
-      val item1 = orderQueries.insertOrderItemPrice(
-        orderId = order1,
-        menuItemId = menu1,
-        price = 10.money.toCents(),
-        datetime = Instant.now(),
-      ).id()
-      val item2 = orderQueries.insertOrderItemPrice(
-        orderId = order1,
-        menuItemId = menu2,
-        price = 5.money.toCents(),
-        datetime = Instant.now(),
-      ).id()
-
-      orderQueries.insertOrderItem(customerId = user1, quantity = 2, orderItemPriceId = item1, "")
-      orderQueries.insertOrderItem(customerId = user1, quantity = 2, orderItemPriceId = item2, "")
-      orderQueries.insertOrderItem(customerId = user2, quantity = 2, orderItemPriceId = item1, "")
-
-      //Only user1 ordered
-      val order2 = orderQueries.insert(restaurantId = 1, createdAt = Instant.now()).id()
-      val item3 = orderQueries.insertOrderItemPrice(
-        orderId = order2,
-        menuItemId = menu1,
-        price = 10.money.toCents(),
-        datetime = Instant.now(),
-      ).id()
-      orderQueries.insertOrderItem(customerId = user1, quantity = 5, orderItemPriceId = item3, "")
-
-      //Only user1 ordered, paid for his share, 1 pay canceled and 1 completed
-      val order3 = orderQueries.insert(restaurantId = 1, createdAt = Instant.now()).id()
-      val item4 = orderQueries.insertOrderItemPrice(
-        orderId = order3,
-        menuItemId = menu1,
-        price = 10.money.toCents(),
-        datetime = Instant.now(),
-      ).id()
-      orderQueries.insertOrderItem(customerId = user1, quantity = 2, orderItemPriceId = item4, "")
-      val payment1 = paymentQueries.insertPayment(
-        userId = user1,
-        amount = 10.money.toCents(),
-        createdAt = Instant.now(),
-        status = PaymentStatus.Canceled.key,
-        direction = PaymentDirection.Credit.key,
-      ).id()
-      val invoice1 = paymentQueries.insertInvoice(
-        paymentId = payment1,
-        totalOwed = 10.money.toCents(),
-        totalPaid = 0.money.toCents()
-      ).id()
-      paymentQueries.insertInvoiceItem(
-        invoiceId = invoice1,
-        orderId = order3,
-        amountPaid = 0.money.toCents(),
-        amountOwed = 10.money.toCents()
-      )
-      val payment2 = paymentQueries.insertPayment(
-        userId = user1,
-        amount = 10.money.toCents(),
-        createdAt = Instant.now(),
-        status = PaymentStatus.Completed.key,
-        direction = PaymentDirection.Credit.key
-      ).id()
-      val invoice2 = paymentQueries.insertInvoice(
-        paymentId = payment2,
-        totalOwed = 10.money.toCents(),
-        totalPaid = 0.money.toCents()
-      ).id()
-      paymentQueries.insertInvoiceItem(
-        invoiceId = invoice2,
-        orderId = order3,
-        amountPaid = 10.money.toCents(),
-        amountOwed = 0
-      )
-
-      //Only user2 ordered, User1 paid for whole order and was reimbursed
-      val order4 = orderQueries.insert(restaurantId = 1, createdAt = Instant.now()).id()
-      orderQueries.updatePayer(payerId = user1, id = order4)
-      val item5 = orderQueries.insertOrderItemPrice(
-        orderId = order4,
-        menuItemId = menu1,
-        price = 10.money.toCents(),
-        datetime = Instant.now(),
-      ).id()
-      orderQueries.insertOrderItem(customerId = user2, quantity = 2, orderItemPriceId = item5, "")
-      val payment3 = paymentQueries.insertPayment(
-        userId = user1,
-        amount = 10.money.toCents(),
-        createdAt = Instant.now(),
-        status = PaymentStatus.Completed.key,
-        direction = PaymentDirection.Credit.key,
-      ).id()
-      val invoice3 = paymentQueries.insertInvoice(
-        paymentId = payment3,
-        totalOwed = 0.money.toCents(),
-        totalPaid = 10.money.toCents()
-      ).id()
-      paymentQueries.insertInvoiceItem(invoice3, order4, 10.money.toCents(), 0)
-
-      val order5 = orderQueries.insert(restaurantId = 1, createdAt = Instant.now()).id()
-      orderQueries.updatePayer(payerId = user1, id = order5)
-      val item6 = orderQueries.insertOrderItemPrice(
-        orderId = order5,
-        menuItemId = menu1,
-        price = 10.money.toCents(),
-        datetime = Instant.now(),
-      ).id()
-      orderQueries.insertOrderItem(customerId = user2, quantity = 4, orderItemPriceId = item6, "")
-      val payment4 = paymentQueries.insertPayment(
-        userId = user1,
-        amount = 40.money.toCents(),
-        createdAt = Instant.now(),
-        status = PaymentStatus.Canceled.key,
-        direction = PaymentDirection.Credit.key,
-      ).id()
-      val invoice4 = paymentQueries.insertInvoice(
-        paymentId = payment4,
-        totalOwed = 40.money.toCents(),
-        totalPaid = 0.money.toCents()
-      ).id()
-      paymentQueries.insertInvoiceItem(
-        invoiceId = invoice4,
-        orderId = order5,
-        amountPaid = 0.money.toCents(),
-        amountOwed = 40.money.toCents()
-      )
+      if (BuildConfig.DEBUG) seed()
       return QueryResult.Unit
     }
   }
+}
+
+private fun TalabiyaDatabase.seed() {
+
+  fun Any.id() = orderQueries.lastInsertRowId().executeAsOne()
+
+  val user1 = userQueries.insert(name = "User 1").id()
+  val user2 = userQueries.insert(name = "User 2").id()
+  val user3 =  userQueries.insert(name = "User 3").id()
+
+  restaurantQueries.insert(name = "Restaurant 1")
+
+  menuItemQueries.insert(restaurantId = 1, name = "Item 1", category = "Pastry")
+  menuItemQueries.insert(restaurantId = 1, name = "Item 2", category = "Pastry")
+  menuItemQueries.insert(restaurantId = 1, name = "Item 3", category = "Pastry")
+
+  val menu1 = menuItemQueries.insertPrice(
+    menuItemId = 1,
+    datetime = Instant.now(),
+    price = 10.money.toCents()
+  ).id()
+
+  val menu2 = menuItemQueries.insertPrice(
+    menuItemId = 2,
+    datetime = Instant.now(),
+    price = 5.money.toCents()
+  ).id()
+
+  //Both users ordered, user1 paid for whole order.
+  val order1 = orderQueries.insert(restaurantId = 1, createdAt = Instant.now()).id()
+
+  orderQueries.updatePayer(user1, order1)
+  val item1 = orderQueries.insertOrderItemPrice(
+    orderId = order1,
+    menuItemId = menu1,
+    price = 10.money.toCents(),
+    datetime = Instant.now(),
+  ).id()
+  val item2 = orderQueries.insertOrderItemPrice(
+    orderId = order1,
+    menuItemId = menu2,
+    price = 5.money.toCents(),
+    datetime = Instant.now(),
+  ).id()
+
+  orderQueries.insertOrderItem(customerId = user1, quantity = 2, orderItemPriceId = item1, "")
+  orderQueries.insertOrderItem(customerId = user1, quantity = 2, orderItemPriceId = item2, "")
+  orderQueries.insertOrderItem(customerId = user2, quantity = 2, orderItemPriceId = item1, "")
+
+  //Only user1 ordered
+  val order2 = orderQueries.insert(restaurantId = 1, createdAt = Instant.now()).id()
+  val item3 = orderQueries.insertOrderItemPrice(
+    orderId = order2,
+    menuItemId = menu1,
+    price = 10.money.toCents(),
+    datetime = Instant.now(),
+  ).id()
+  orderQueries.insertOrderItem(customerId = user1, quantity = 5, orderItemPriceId = item3, "")
+
+  //Only user1 ordered, paid for his share, 1 pay canceled and 1 completed
+  val order3 = orderQueries.insert(restaurantId = 1, createdAt = Instant.now()).id()
+  val item4 = orderQueries.insertOrderItemPrice(
+    orderId = order3,
+    menuItemId = menu1,
+    price = 10.money.toCents(),
+    datetime = Instant.now(),
+  ).id()
+  orderQueries.insertOrderItem(customerId = user1, quantity = 2, orderItemPriceId = item4, "")
+  val payment1 = paymentQueries.insertPayment(
+    userId = user1,
+    amount = 10.money.toCents(),
+    createdAt = Instant.now(),
+    status = PaymentStatus.Canceled.key,
+    direction = PaymentDirection.Credit.key,
+  ).id()
+  val invoice1 = paymentQueries.insertInvoice(
+    paymentId = payment1,
+    totalOwed = 10.money.toCents(),
+    totalPaid = 0.money.toCents()
+  ).id()
+  paymentQueries.insertInvoiceItem(
+    invoiceId = invoice1,
+    orderId = order3,
+    amountPaid = 0.money.toCents(),
+    amountOwed = 10.money.toCents()
+  )
+  val payment2 = paymentQueries.insertPayment(
+    userId = user1,
+    amount = 10.money.toCents(),
+    createdAt = Instant.now(),
+    status = PaymentStatus.Completed.key,
+    direction = PaymentDirection.Credit.key
+  ).id()
+  val invoice2 = paymentQueries.insertInvoice(
+    paymentId = payment2,
+    totalOwed = 10.money.toCents(),
+    totalPaid = 0.money.toCents()
+  ).id()
+  paymentQueries.insertInvoiceItem(
+    invoiceId = invoice2,
+    orderId = order3,
+    amountPaid = 10.money.toCents(),
+    amountOwed = 0
+  )
+
+  //Only user2 ordered, User1 paid for whole order and was reimbursed
+  val order4 = orderQueries.insert(restaurantId = 1, createdAt = Instant.now()).id()
+  orderQueries.updatePayer(payerId = user1, id = order4)
+  val item5 = orderQueries.insertOrderItemPrice(
+    orderId = order4,
+    menuItemId = menu1,
+    price = 10.money.toCents(),
+    datetime = Instant.now(),
+  ).id()
+  orderQueries.insertOrderItem(customerId = user2, quantity = 2, orderItemPriceId = item5, "")
+  val payment3 = paymentQueries.insertPayment(
+    userId = user1,
+    amount = 10.money.toCents(),
+    createdAt = Instant.now(),
+    status = PaymentStatus.Completed.key,
+    direction = PaymentDirection.Credit.key,
+  ).id()
+  val invoice3 = paymentQueries.insertInvoice(
+    paymentId = payment3,
+    totalOwed = 0.money.toCents(),
+    totalPaid = 10.money.toCents()
+  ).id()
+  paymentQueries.insertInvoiceItem(invoice3, order4, 10.money.toCents(), 0)
+
+  val order5 = orderQueries.insert(restaurantId = 1, createdAt = Instant.now()).id()
+  orderQueries.updatePayer(payerId = user1, id = order5)
+  val item6 = orderQueries.insertOrderItemPrice(
+    orderId = order5,
+    menuItemId = menu1,
+    price = 10.money.toCents(),
+    datetime = Instant.now(),
+  ).id()
+  orderQueries.insertOrderItem(customerId = user2, quantity = 4, orderItemPriceId = item6, "")
+  val payment4 = paymentQueries.insertPayment(
+    userId = user1,
+    amount = 40.money.toCents(),
+    createdAt = Instant.now(),
+    status = PaymentStatus.Canceled.key,
+    direction = PaymentDirection.Credit.key,
+  ).id()
+  val invoice4 = paymentQueries.insertInvoice(
+    paymentId = payment4,
+    totalOwed = 40.money.toCents(),
+    totalPaid = 0.money.toCents()
+  ).id()
+  paymentQueries.insertInvoiceItem(
+    invoiceId = invoice4,
+    orderId = order5,
+    amountPaid = 0.money.toCents(),
+    amountOwed = 40.money.toCents()
+  )
 }
